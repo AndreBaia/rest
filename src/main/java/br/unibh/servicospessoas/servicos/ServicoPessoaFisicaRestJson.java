2f8dc0bc0d47cf965c3b38a1709072fcc72bd2f5
AndreBaia <mdacriativa@gmail.com>
2015-10-05 18:09:37 -0300

    Exercicio Completo

--- src/main/java/br/unibh/servicospessoas/servicos/ServicoPessoaFisicaRestJson.java
+++ src/main/java/br/unibh/servicospessoas/servicos/ServicoPessoaFisicaRestJson.java
@@ -3,7 +3,11 @@ package br.unibh.servicospessoas.servicos;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,6 +22,7 @@ import br.unibh.servicospessoas.persistencia.PessoaFisicaDAO;
 @Path("/pessoafisica")
 public class ServicoPessoaFisicaRestJson {
 
+	// Listar tudo
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/listar")
@@ -26,15 +31,51 @@ public class ServicoPessoaFisicaRestJson {
 		return dao.findAll();
 	}
 
-	// Método buscar
-	 @GET
-	    @Produces(MediaType.APPLICATION_JSON)
-	    @Path("/{id}")
-	    public PessoaFisica find(@PathParam("id") int id){
-			PessoaFisicaDAO dao = new PessoaFisicaDAO();
-	    	return dao.find(new Long(id));	
-	    }
-	 
-	
-    
+	// Buscar
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public PessoaFisica find(@PathParam("id") int id) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		return dao.find(new Long(id));
+	}
+
+	// Criar
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/salvar")
+	public PessoaFisica salvar(PessoaFisica p) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		dao.insert(p);
+		dao.update(p);
+		return dao.find(p.getCpf());
+	}
+
+	// Atualizar
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public PessoaFisica atualizar(@PathParam("id") int id, PessoaFisica p) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		dao.update(p);
+		return dao.find(new Long(id));
+	}
+
+	// Delete
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public void deletarJSON(@PathParam("id") int id) {
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		PessoaFisica pf = dao.find(new Long(id));
+		dao.delete(pf);
+		PessoaFisica pf2 = dao.find(new Long(id));
+		if (pf2 == null) {
+			System.out.println("Pessoa Fisica foi deletada com sucesso");
+		} else {
+			System.out.println("Erro ao deletar a Pessoa Fisica");
+		}
+	}
+
 }
--- src/main/java/br/unibh/servicospessoas/servicos/ServicoPessoaJuridicaRestJson.java
+++ src/main/java/br/unibh/servicospessoas/servicos/ServicoPessoaJuridicaRestJson.java
@@ -2,23 +2,79 @@ package br.unibh.servicospessoas.servicos;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import br.unibh.servicospessoas.entidades.PessoaJuridica;
 import br.unibh.servicospessoas.persistencia.PessoaJuridicaDAO;
 
+//A classe Java será hospedada no caminho URI "/pessoajuridica"
+//Método listar
+
 @Path("/pessoajuridica")
 public class ServicoPessoaJuridicaRestJson {
-	
+
+	// Listar Tudo
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/listar")
-	public List<PessoaJuridica> listar(){
+	public List<PessoaJuridica> listar() {
 		PessoaJuridicaDAO dao = new PessoaJuridicaDAO();
 		return dao.findAll();
+	}
+
+	// Buscar
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public PessoaJuridica find(@PathParam("id") int id) {
+		PessoaJuridicaDAO dao = new PessoaJuridicaDAO();
+		return dao.find(new Long(id));
+	}
+
+	// Criar
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/salvar")
+	public PessoaJuridica salvar(PessoaJuridica p) {
+		PessoaJuridicaDAO dao = new PessoaJuridicaDAO();
+		dao.insert(p);
+		dao.update(p);
+		return dao.find(p.getCnpj());
+	}
+
+	// Atualizar
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public PessoaJuridica atualizar(@PathParam("id") int id, PessoaJuridica p) {
+		PessoaJuridicaDAO dao = new PessoaJuridicaDAO();
+		dao.update(p);
+		return dao.find(new Long(id));
+	}
+
+	// Delete
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public void deletarJSON(@PathParam("id") int id) {
+		PessoaJuridicaDAO dao = new PessoaJuridicaDAO();
+		PessoaJuridica pf = dao.find(new Long(id));
+		dao.delete(pf);
+		PessoaJuridica pf2 = dao.find(new Long(id));
+		if (pf2 == null) {
+			System.out.println("Pessoa Juridica foi deletada com sucesso");
+		} else {
+			System.out.println("Erro ao deletar a Pessoa Juridica");
+		}
 	}
 
 }
